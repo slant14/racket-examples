7159d1be9207b6c3d3a097c56af37a652371746f
Mahmoud Naderi <32930400+slant14@users.noreply.github.com>
2024-09-19 13:52:19 +0300

    Create queen-attack.rkt

---
+++ queen-attack.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+(define (attacks2? q1 q2)
+  (define attack-checker
+    (list
+     (lambda (q1 q2) (= (first q1) (first q2)))
+     (lambda (q1 q2) (= (second q1) (second q2)))
+     (lambda (q1 q2) (= (abs (- (first q1) (first q2)))
+                        (abs (- (second q1) (second q2)))))
+     ))
+  (ormap (lambda (f) (f q1 q2)) attack-checker))
+
+(define (attacks? q1 q2)
+  (or
+   (= (first q1) (first q2))
+   (= (second q1) (second q2))
+   (= (abs (- (first q1) (first q2)))
+                        (abs (- (second q1) (second q2))))))
+
+(attacks? '(1 5) '(1 3))
+(attacks? '(5 1) '(3 1))
+(attacks? '(1 2) '(3 4))
+(attacks? '(1 5) '(2 3))
+
+
+(define (attacks-any? q qs)
+  (ormap (lambda (other-q) (attacks? q other-q)) qs))
+
+
+(define (no-attacks? qs)
+  (not (ormap (lambda (q)
+                (attacks-any? q (cdr (member q qs))))
+              qs)))
+
+
+(define q1 '(1 1))
+(define q2 '(3 2))
+(define q3 '(6 4))
+(define q4 '(5 7))
+(define qs1 (list q1 q2 q3 q4))
+
+(no-attacks? qs1)
